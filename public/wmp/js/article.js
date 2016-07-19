6494bcace5497914a28551a8106a85ae761f8628
kevin1220 <1079936039@qq.com>
2016-07-19 10:29:42 +0800

    通过vue来渲染从后台获取到的公众号文章的信息

--- public/wmp/js/article.js
+++ public/wmp/js/article.js
@@ -1,33 +1,15 @@
-var vue1 = new Vue({
-    el: '#articles',
-    data: {
-        articles: [],
-        page: 1,
-        seemore: true
-    },
-    methods: {
-        getmore: function() {
-            if (data.length < 20) {
-                vue1.seemore = false;
-            } else {
-                page++;
-                getarticles(page);
-            }
-
-        }
-    }
-});
-
 $(function() {
     getarticles(1);
     //通过Ajax获取后台返回来的文章信息
     function getarticles(page) {
         var page = page;
         $.ajax({
-            url: '/wmp/article/getarticles?page='+page,
+            url: '/wmp/article/getarticles?page=' + page,
             type: 'GET',
             success: function(data) {
-
+                if (data.item.length < 20) {
+                    vue1.seemore = false;
+                }
                 render(data);
             },
             error: function(err) {
@@ -35,10 +17,23 @@ $(function() {
             }
         });
     }
-
     //渲染文章内容部分
+    var vue1 = new Vue({
+        el: '#articles',
+        data: {
+            articles: [],
+            page: 1,
+            seemore: true
+        },
+        methods: {
+            getmore: function() {
+                this.page++;
+                getarticles(this.page);
+            }
+        }
+    });
     function render(newarticles) {
-        vue1.articles.concat(newarticles);
+        vue1.articles = vue1.articles.concat(newarticles.item)
     }
 
 });
